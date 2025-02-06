6475780abefc710757d53f9d384bb1f19513f73b
Bryan Dang <bryanvdang@gmail.com>
2025-02-06 12:11:49 -0700

    create event emitter

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,11 +7,15 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() wasSelected = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
-  onSelectUser() {}
+  onSelectUser() {
+    this.wasSelected.emit(this.id);
+  }
 }
