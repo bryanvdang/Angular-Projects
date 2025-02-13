c1442137c88dbc6501dfd34931db48bab8baf486
Bryan Dang <bryanvdang@gmail.com>
2025-02-12 22:51:34 -0700

    use ng-content from shared card component to style user component and task component

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Task } from './task.model';
+import { CardComponent } from "../../share/card/card.component";
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
